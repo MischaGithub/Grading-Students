e71efa3ceafac3e48742f9b2854aa73075e37757
MischaGithub <mischa.achmat120212@gmail.com>
2020-04-23 13:10:41 -0700

    Stack exercise

---
+++ stack.py
@@ -0,0 +1,17 @@
+
+#Program performing the stack implementation
+
+#Creating an empty stack
+my_stack = []
+
+#Creatng a function to add to the stack based on the users history
+def add_to_stack(my_history):
+    """
+    >>> my_history["google.co.za", "money.co.za", "cars.co.za", "fb.com"]
+    google.co.za
+    money.co.za
+    cars.co.za
+    fb.com
+    """
+
+
